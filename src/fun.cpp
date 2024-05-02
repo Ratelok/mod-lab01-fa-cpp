23c3f5a6c8917e1ffc17966d6fec3bfbf005485d
Ratelok <120968192+Ratelok@users.noreply.github.com>
2024-05-02 13:30:43 +0300

    11

--- src/fun.cpp
+++ src/fun.cpp
@@ -5,54 +5,70 @@
 #include <iostream>
 #include <string>
 
-unsigned int faStr1(const char *str) {
+unsigned int faStr1(const char *str)
+{
     unsigned int count = 0;
     bool inWord = false;
     bool hasDigit = false;
 
-    while (*str != '\0') {
-        if (isspace(*str)) {
-            if (inWord && !hasDigit) {
+    while (*str != '\0')
+    {
+        if (isspace(*str))
+        {
+            if (inWord && !hasDigit)
+            {
                 count++;
             }
             inWord = false;
             hasDigit = false;
-        } else {
-            if (!inWord) {
+        }
+        else
+        {
+            if (!inWord)
+            {
                 inWord = true;
             }
-            if (isdigit(*str)) {
+            if (isdigit(*str))
+            {
                 hasDigit = true;
             }
         }
         str++;
     }
 
-    if (inWord && !hasDigit) {
+    if (inWord && !hasDigit)
+    {
         count++;
     }
 
     return count;
 }
 
-unsigned int faStr2(const char* str) {
+unsigned int faStr2(const char *str)
+{
     unsigned int count = 0;
     bool inWord = false;
-    for (; *str; ++str) {
-        if (std::isspace(*str)) {
+    for (; *str; ++str)
+    {
+        if (std::isspace(*str))
+        {
             inWord = false;
         }
-        else if (!inWord) {
+        else if (!inWord)
+        {
             inWord = true;
             bool startsWithUppercase = std::isupper(*str);
             bool onlyLowercase = true;
-            for (const char* p = str + 1; !std::isspace(*p) && *p; ++p) {
-                if (!std::islower(*p)) {
+            for (const char *p = str + 1; !std::isspace(*p) && *p; ++p)
+            {
+                if (!std::islower(*p))
+                {
                     onlyLowercase = false;
                     break;
                 }
             }
-            if (startsWithUppercase && onlyLowercase) {
+            if (startsWithUppercase && onlyLowercase)
+            {
                 ++count;
             }
         }
@@ -60,32 +76,40 @@ unsigned int faStr2(const char* str) {
     return count;
 }
 
-unsigned int faStr3(const char *str) {
+unsigned int faStr3(const char *str)
+{
     unsigned int sum = 0;
     unsigned int count = 0;
     unsigned int len = 0;
 
-    while (*str != '\0') {
-        if (isspace(*str)) {
-            if (len > 0) {
+    while (*str != '\0')
+    {
+        if (isspace(*str))
+        {
+            if (len > 0)
+            {
                 sum += len;
                 count++;
                 len = 0;
             }
-        } else {
+        }
+        else
+        {
             len++;
         }
         str++;
     }
 
-    if (len > 0) {
+    if (len > 0)
+    {
         sum += len;
         count++;
     }
 
-    if (count == 0) {
+    if (count == 0)
+    {
         return 0;
     }
 
-    return (unsigned int) (sum / count + 0.5);
+    return (unsigned int)(sum / count + 0.5);
 }
